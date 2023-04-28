ee6317a53e9c42de7dc2feb29af1f182b3db5850
Zachary <gray.zachary1994@gmail.com>
2023-04-28 08:27:36 -0500

    Added recipe card, and hover effect over buttons.

--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import Login from "./Login";
+import RecipeCard from "../components/recipeCard";
 
 export default function LoginPage() {
     return (
         <>
             <Navbar />
             <Login />
+            <div className="recipes">
+            <RecipeCard></RecipeCard>
+            </div>
         </>
     )
 }
