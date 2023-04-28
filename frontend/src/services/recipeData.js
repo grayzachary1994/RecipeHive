d3fff1c683b520b9ef043137aa8f796a1eec0d9c
Matthew Creek <matthew.creek27@gmail.com>
2023-04-28 14:57:14 -0500

    made dummy data in services for testing purposes

---
+++ frontend/src/services/recipeData.js
@@ -0,0 +1,170 @@
+
+
+const data = [
+    {
+        id: 1,
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2FPhoto%2FRecipes%2F2021-12-spaghetti%2F211208_ApartmentTherapy_Spaghetti_0172',
+        title: 'Spaghetti',
+        description: 'Italian dish featuring long noodles in a tomato-based sauce',
+        ingredients: [
+            "1 medium yellow onion",
+            "3 cloves garlic",
+            "1 tablespoon olive oil",
+            "1 pound lean ground beef",
+            "1 1/2 teaspoons kosher salt, divided, plus more for the pasta water",
+            "1/2 teaspoon dried oregano",
+            "1/4 teaspoon freshly ground black pepper",
+            "1/4 teaspoon red pepper flakes (optional)",
+            "1 (28-ounce) can crushed tomatoes",
+            "1 pound dried spaghetti",
+            "2 teaspoons balsamic vinegar",
+            "Freshly grated Parmesan cheese, for serving" 
+        ],
+        directions: [
+            "Finely chop 1 medium yellow onion and mince 3 garlic cloves. Heat 1 tablespoon olive oil in a large high-sided skillet over medium-high heat until shimmering. Add the onion, garlic, 1 pound lean ground beef, 1 teaspoon of the kosher salt, 1/2 teaspoon dried oregano, 1/4 teaspoon black pepper, and 1/4 teaspoon red pepper flakes, if using. Cook, breaking the beef up into small pieces with a wooden spoon, until the beef is cooked through, 5 to 7 minutes.",
+            "Carefully pour in 1 (28-ounce) can crushed tomatoes and season with the remaining 1/2 teaspoon kosher salt. Bring to a boil. Reduce the heat to medium-low and gently simmer uncovered until the sauce is slightly thickened, about 20 minutes.",
+            "Meanwhile, bring a large pot of salted water to a boil. Add 1 pound dried spaghetti and cook for 1 minute less than the package instructions for al dente, about 9 minutes. Reserve 1 cup of the pasta cooking water and drain the spaghetti.",
+            "Return the pasta to the pot. Add the sauce, reserved pasta cooking water, and 2 teaspoons balsamic vinegar, and toss to combine. Bring to a simmer over medium heat, tossing continuously, until the sauce coats the pasta and everything is evenly combined, 1 to 2 minutes. Serve in bowls topped with lots of grated Parmesan cheese."
+        ],
+        time: "28-35 minutes",
+        type: 'meal'
+    },
+    {
+        id: 2,
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_730,h_913/k%2FPhoto%2FRecipes%2F2020-09-Cast-Iron-Pressed-Pizza-Sandwich%2FCast-Iron-Pressed-Pizza-Sandwiches176',
+        title: 'Cast Iron Pressed Pizza Sandwiches',
+        description: 'Panini-inspired sandwich made with pizza ingredients',
+        ingredients: [
+            '1 1/2 tablespoons unsalted butter, at room temperature',
+            '4 slices white sandwich bread',
+            '1/2 teaspoon dried oregano',
+            '1/2 teaspoon garlic powder',
+            '6 tablespoons pizza or marinara sauce',
+            '1/2 cup shredded low-moisture mozzarella cheese',
+            '18 mini or 12 regular pepperoni slices',
+            '1/2 teaspoon red pepper flakes (optional)'
+        ],
+        directions: [
+            'Place 4 slices white sandwich bread on a work surface. Spread 1 1/2 tablespoons unsalted butter onto the bread. Sprinkle with 1/2 teaspoon dried oregano and 1/2 teaspoon garlic powder.',
+            'Flip the slices over and spread with 6 tablespoons pizza sauce. Divide 1/2 cup shredded mozzarella cheese among 2 of the bread slices, then top with 18 mini or 12 regular pepperoni slices. Sprinkle the pepperoni with 1/2 teaspoon red pepper flakes if desired. Close the sandwiches with the remaining 2 bread slices sauce-side down.',
+            'Option 1: Stovetop cooking with a skillet. Heat a large skillet over medium heat. Add the sandwiches and place a cast iron skillet or other heavy-bottomed pot or pan on top of the sandwiches as a press. Cook until golden brown, about 2 minutes. Uncover and flip the sandwiches. Place the second pan on top of the sandwiches again and cook until the second side is golden brown, about 2 minutes more.',
+            'Option 2: Pie iron cooking. Open a cast iron pie iron and place 1 sandwich inside. Close it completely over the sandwich and latch it shut.',
+            'Place the iron on top of the embers of an open fire. Cook until the bottom is golden brown, 2 to 3 minutes. Flip the iron and cook until the second side is golden brown, about 2 minutes more. Remove from the heat, place on a heat resistant surface, and let cool for 3 to 5 minutes while still shut. The iron will still be extremely hot even after cooling, so be careful.',
+            'Carefully unlatch the cast iron square and pop the sandwich onto a plate. Repeat cooking the remaining sandwich.'
+        ],
+        time: '5-10 minutes',
+        type: 'snack'
+    },
+    {
+        id: 3,
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Farchive%2F842a29791154a85706a21874c6fc176235cb5df1',
+        title: 'Scrambled Eggs with Green Peppers and Mushrooms',
+        description: 'Cheesy eggs with fresh veggies',
+        ingredients: [
+            '2 teaspoons olive oil',
+            '1/4 cup diced yellow onion',
+            '1/4 cup diced green bell pepper',
+            '1/2 cup diced button mushrooms',
+            '1 cup arugula or baby spinach',
+            '4 large eggs, beaten',
+            '1/4 cup shredded sharp cheddar',
+            'Hot sauce, optional',
+        ],
+        directions: [
+            'Warm the olive oil in a nonstick skillet over medium heat. Add the onion, green bell pepper, and mushrooms. Cook, stirring occasionally, until the vegetables begin to soften, about 5 minutes.',
+            'Add the arugula and continue cooking, just until it begins to wilt. Add the eggs and stir. Using a spatula, push the eggs from the sides of the skillet into the center. When curds begin to form, add the cheddar and continue to move the eggs around the pan until just set.',
+            'Serve immediately, with hot sauce if you like.'
+        ],
+        time: '5 minutes',
+        type: 'breakfast'
+    },
+    {
+        id: 4,
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Farchive%2Fae2796ddeae22d57b3923adbcb4b28f2e0825a78',
+        title: 'Spaghetti Squash Pad Thai',
+        description: 'Classic pad thai with a modern twist',
+        ingredients: [
+            '2 tablespoons tamarind paste (or rice wine vinegar, see Recipe Note)',
+            '2 tablespoons fish sauce',
+            '2 tablespoons palm sugar or brown sugar',
+            '2 to 4 tablespoons water, to thin',
+            '1/2 medium spaghetti squash (from a 3-pound squash)',
+            '2 1/2 tablespoons peanut oil',
+            '8 ounces extra-firm tofu, diced',
+            '2 tablespoons cornstarch',
+            '1/2 medium yellow onion, thinly sliced',
+            '2 large eggs, whisked',
+            '2 cloves garlic, minced',
+            '4 scallions, cut into 1- to 2-inch pieces',
+            '1/2 cup bean sprouts, plus more to serve',
+            '2 tablespoons chopped peanuts, to serve',
+            'Lime wedges, to serve',
+            'Cilantro, to serve',
+            'Red pepper flakes, to serve',
+        ],
+        directions: [
+            'Whisk together the tamarind paste, fish sauce, and palm sugar for the sauce. Add 2 tablespoons of water, to thin out the sauce. Microwave on high heat for 30 seconds and whisk until everything is combined into a thin sauce. The sauce should taste very strong, but still palatable; add more water if needed to reach a good balance of tartness and pungency. Measure out about 1/4 cup to be used for this recipe; the remaining sauce will keep refrigerated for several weeks.',
+            'Cut the squash in half; save one half for another purpose. Prepare the other half, either in the microwave (15 minutes) or in the oven (30 to 45 minutes). When cooked, shred the inside with a fork and set aside. You should have 3 to 4 cups of spaghetti squash tendrils.',
+            'Toss the tofu in the cornstarch until all the cubes are evenly coated with a gummy layer of cornstarch. Set aside while you prep the rest of the ingredients; make sure all the ingredients are prepped before you begin cooking. Place a large dinner plate next to the stove to hold ingredients as they come out of the wok.',
+            'Heat a wok or large skillet over high heat until a flick of water dissolves almost instantly on the surface. Add a tablespoon of peanut oil and quickly swirl the pan to coat. Add the tofu and stir-fry until golden on all sides, 2 to 3 minutes. Transfer the cooked tofu to a plate. (Adjust the heat as needed if your pan starts smoking.)',
+            'Warm another half tablespoon of peanut oil in the pan and add the onions. Cook until the onions are just starting to soften and show golden color, 3 to 4 minutes. Transfer to the plate with the tofu. (Adjust the heat as needed if your pan starts smoking.)',
+            'Warm another half tablespoon of peanut oil in the pan and swirl to coat the bottom. Whisk the eggs one more time and then pour them into the bottom of the pan. Cook, tilting the pan to create a very thin omelet. When the eggs are almost set, begin nudging and cutting them with your spatula to create big curds. Transfer the cooked eggs to the plate with the tofu and onion. (Adjust the heat as needed if your pan starts smoking.)',
+            'Warm the last half tablespoon of peanut oil in the pan and add the garlic. Fry until the garlic is fragrant and golden, about 10 seconds. Add all of the spaghetti squash and spread into a single layer. Cook for 30 seconds or so, then stir the squash and spread it back out again. Repeat a few times until the squash is warmed and beginning to show golden, roasted color.',
+            'Drizzle 2 tablespoons of the pad Thai sauce around the outside edge of the pan, then stir it into the squash. Continue stirring until the sauce evenly coats all the squash. Give it a quick taste and add up to 2 tablespoons additional sauce if needed.',
+            'Add the scallions and bean sprouts to the pan with the squash, and stir to combine. Add the tofu, onions, and egg back to the pan and stir to combine. Taste again, adding additional sauce if needed.',
+            'Transfer the pad Thai to a large serving plate and top with chopped peanuts, cilantro, red pepper flakes, and lime wedges. Serve immediately while still very hot. Leftovers reheat well and will keep for up to a week in the fridge.'
+        ],
+        time: '90 minutes',
+        type: 'meal'
+    },
+    {
+        id: 5,
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2022-02-Sesame-Noodles%2FIMG_7199',
+        title: 'Cold Sesame Nooodles',
+        description: 'Chewy noodles tossed in a sesame-peanut sauce',
+        ingredients: [
+            '2 cloves garlic',
+            '1 (1-inch) piece ginger',
+            '2 medium scallions',
+            '1/4 cup smooth peanut butter',
+            '1 tablespoon Chinese sesame paste (see Recipe Notes)',
+            '2 tablespoons soy sauce',
+            '2 tablespoons rice vinegar',
+            '2 tablespoons hot water',
+            '1 tablespoon granulated sugar',
+            '1 tablespoon toasted sesame oil',
+            '1 teaspoon Asian chili-garlic paste, such as sambal oelek',
+            '1/3 medium English cucumber (about 4 ounces, optional)',
+            '8 ounces thin dried Chinese wheat noodles, preferably Taiwanese kuan miao'
+        ],
+        directions: [
+            'Bring a large saucepan of water to a boil over medium-high heat. Meanwhile, make the sauce.',
+            'Mince 2 garlic cloves. Peel and mince 1 (1-inch) piece ginger until you have 1 teaspoon. Place both in a large bowl. Thinly slice 2 medium scallions; add the scallion white and light green parts to the bowl and reserve the dark green parts for garnish.',
+            'Add 1/4 cup peanut butter, 1 tablespoon Chinese sesame paste, 2 tablespoons soy sauce, 2 tablespoons rice vinegar, 2 tablespoons hot water, 1 tablespoon granulated sugar, 1 tablespoon toasted sesame oil, and 1 teaspoon Asian chili-garlic paste to the bowl and whisk until creamy and combined (it will look curdled at first). Cut 1/3 medium English cucumber into matchsticks (about 1 cup) and add to the bowl of sauce if desired.',
+            'Add 8 ounces dried thin Chinese wheat noodles to the boiling water and cook according to package directions until tender. Meanwhile, coarsely chop 2 tablespoons roasted peanuts if desired.',
+            'When the noodles are ready, drain and rinse under cold water until cooled. Drain again, shaking off the excess water. Add to the sauce and toss until evenly coated. Garnish with the reserved scallion greens, peanuts, and 1 teaspoon sesame seeds if desired.'
+        ],
+        time: '20-25 minutes',
+        type: 'snack'
+    },
+    {
+        id: 6,
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_730,h_913/k%2FPhoto%2FRecipes%2F2022-03_Peanut-Butter-Banana-Smoothie%2F2022-03-16_ATK-0324',
+        title: 'Peanut Butter Banana Smoothie',
+        description: 'A cool and creamy peanut butter and banana smoothie',
+        ingredients: [
+            '1 medium banana (about 5 ounces)',
+            '1 cup unsweetened vanilla or plain nondairy milk or whole milk',
+            '2 tablespoons peanut butter',
+            'Optional: 1/4 teaspoon ground cinnamon, 1 tablespoon honey or maple syrup, 1/2 teaspoon vanilla extract, 2 tablespoons cocoa powder'
+        ],
+        directions: [
+            'Peel 1 large banana, then tear or slice into 1-inch pieces. Arrange the pieces on a parchment-lined baking sheet, and freeze until solid (at least 2 hours).',
+            'Add 1 cup unsweetened vanilla or plain almond milk or whole milk, 2 tablespoons creamy peanut butter, any of the optional ingredients (if desired), and the frozen banana pieces to a blender. Blend on high speed until very smooth, stopping to scrape down the sides of the blender as needed, 30 to 60 seconds. Pour into a tall glass and serve immediately.'
+        ],
+        time: '5 minutes',
+        type: 'beverage'
+    },
+]
+
+export default data;
