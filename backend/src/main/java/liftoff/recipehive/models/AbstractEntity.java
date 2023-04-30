9947e24419c5dca22eb794dba3be0060f53213e7
Matthew Creek <matthew.creek27@gmail.com>
2023-04-30 17:56:50 -0500

    register form is submitting to local database

--- backend/src/main/java/liftoff/recipehive/models/AbstractEntity.java
+++ backend/src/main/java/liftoff/recipehive/models/AbstractEntity.java
@@ -1,33 +1,33 @@
-package liftoff.recipehive.models;
-
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.Id;
-
-@MappedSuperclass
-public abstract class AbstractEntity {
-    @Id
-    @GeneratedValue
-    private int id;
-
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AbstractEntity that = (AbstractEntity) o;
-
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-}
+//package liftoff.recipehive.models;
+//
+//
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.MappedSuperclass;
+//import org.springframework.data.annotation.Id;
+//
+//@MappedSuperclass
+//public abstract class AbstractEntity {
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        AbstractEntity that = (AbstractEntity) o;
+//
+//        return id == that.id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return id;
+//    }
+//
+//}
--- frontend/src/pages/register/RegisterForm.js
+++ frontend/src/pages/register/RegisterForm.js
@@ -1,17 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import './register.css';
+import userService from "../../services/UserService";
 
 export default function RegisterForm() {
+
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        verify: ''
+    })
+
+    function handleFormChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    async function handleFormSubmit(event) {
+        try {
+            event.preventDefault();
+            await userService.saveUser(formData);
+            setFormData({
+                username: '',
+                email: '',
+                password: '',
+                verify: ''
+            })
+        }
+        catch(e) {
+            console.log(e)
+        }
+    }
+
+    console.log(formData)
+
     return (
         <div className="register-page">
             <div className="register">
                 <h1 className="register--title">Register</h1>
                 <div className="register--fields">
-                    <input type="text" placeholder="Username" />
-                    <input placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Confirm Password" />
-                    <button className="registerButton">Register</button>
+                    <input 
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        onChange={handleFormChange}
+                        value={formData.username} 
+                    />
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={handleFormChange}
+                        value={formData.email} 
+                    />
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={handleFormChange}
+                        value={formData.password} 
+                    />
+                    <input 
+                        type="password"
+                        name="verify"
+                        placeholder="Confirm Password"
+                        onChange={handleFormChange}
+                        value={formData.verify}
+                    />
+                    <button onClick={handleFormSubmit} className="registerButton">Register</button>
                 </div>
                 <div className="register--links">
                     <a href="www.google.com">Forgot password?</a>
