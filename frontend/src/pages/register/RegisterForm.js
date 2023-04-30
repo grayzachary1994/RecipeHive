a177548294906c5964e037635c952ea2a4f8c8e0
Matthew Creek <matthew.creek27@gmail.com>
2023-04-30 18:30:13 -0500

    added pseudo validation to register form logic

--- frontend/src/pages/register/RegisterForm.js
+++ frontend/src/pages/register/RegisterForm.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import './register.css';
 import userService from "../../services/UserService";
 
 export default function RegisterForm() {
 
+    const navigate = useNavigate();
+
+    const [isMatching, setIsMatching] = useState(true);
     const [formData, setFormData] = useState({
         username: '',
         email: '',
@@ -22,21 +26,32 @@ export default function RegisterForm() {
 
     async function handleFormSubmit(event) {
         try {
-            event.preventDefault();
-            await userService.saveUser(formData);
-            setFormData({
-                username: '',
-                email: '',
-                password: '',
-                verify: ''
-            })
+            if (formData.password !== formData.verify) {
+                event.preventDefault();
+                setIsMatching(false);
+                setFormData(prevFormData => {
+                    return {
+                        ...prevFormData,
+                        password: '',
+                        verify: ''
+                    }
+                })
+            } else {
+                event.preventDefault();
+                await userService.saveUser(formData);
+                setFormData({
+                    username: '',
+                    email: '',
+                    password: '',
+                    verify: ''
+                })
+                navigate('/');
+            }
         }
         catch(e) {
             console.log(e)
         }
     }
-
-    console.log(formData)
 
     return (
         <div className="register-page">
@@ -71,6 +86,7 @@ export default function RegisterForm() {
                         onChange={handleFormChange}
                         value={formData.verify}
                     />
+                    {!isMatching && <p className="error">Passwords do not match!</p>}
                     <button onClick={handleFormSubmit} className="registerButton">Register</button>
                 </div>
                 <div className="register--links">
