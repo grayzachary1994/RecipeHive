7c8cb8aee698306075fd3def9184b068e4ad7292
Matthew Creek <matthew.creek27@gmail.com>
2023-05-03 21:01:57 -0500

    refactored dropdown menu to be inline with design philosophy and file structure

--- frontend/src/pages/components/HamburgerButton.js
+++ frontend/src/pages/components/HamburgerButton.js
@@ -1,29 +1,6 @@
 import React, { useState } from 'react';
-import userService from '../../services/UserService';
-import { useNavigate } from 'react-router-dom';
 
-const DropdownMenu = ({ isOpen }) => {
-
-  const navigate = useNavigate();
-
-  const menuStyle = isOpen ? { display: 'block' } : { display: 'none' };
-
-  function handleLogout(){
-    userService.userLogout()
-    navigate('/login')
-  }
-
-  return (
-    <div className="dropdown-menu" style={menuStyle}>
-      <div className='dropdown-container'>
-        <a href="/">View Cookbook</a>
-        <a href="/add">Add Recipe</a>
-        <p onClick={handleLogout}>Logout</p>
-      </div>
-      
-    </div>
-  );
-};
+import DropdownMenu from './DropdownMenu';
 
 const HamburgerButton = () => {
   const [isOpen, setIsOpen] = useState(false);
