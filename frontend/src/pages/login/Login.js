700f62a5cc0fb0b52bfc5f18cb95f6305856d828
Matthew Creek <matthew.creek27@gmail.com>
2023-05-03 19:59:35 -0500

    user login confirmed working. wip logout

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './login.css';
+import userService from "../../services/UserService";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+
+    function handleFormChange(event){
+        const {name, value} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+    
+    async function handleFormSubmit(event) {
+        try {
+            event.preventDefault();
+            await userService.userLogin(formData);
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
     return (
         <div className="login-page">
             <div className="login">
                 <div className="login--field">
-                    <input placeholder="Username"></input>
-                    <input placeholder="Password"></input>
-                    <button>Log In</button>
+                    <input 
+                        placeholder="Username"
+                        name="username"
+                        onChange={handleFormChange}
+                        value={formData.username}
+                    />
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleFormChange}
+                        value={formData.password}
+                    />
+                    <button onClick={handleFormSubmit}>Log In</button>
                 </div>
                 <div className="login--links">
                     <a href="www.google.com">Forgot password?</a>
--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -5,6 +5,14 @@ class UserService {
     saveUser(user) {
         return axios.post('http://localhost:8080/api/user/add', user)
     }
+
+    userLogin(user) {
+        return axios.post('http://localhost:8080/api/user/login', user)
+    }
+
+    userLogout() {
+        return axios.get('http://localhost:8080/api/user/logout')
+    }
 }
 
 const userService = new UserService();
