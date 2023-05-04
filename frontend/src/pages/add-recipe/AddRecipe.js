0c02e55c2b67b56e46d9b719a5861fc7020ed113
Zachary <gray.zachary1994@gmail.com>
2023-05-04 08:18:37 -0500

    Fixed image preview and set it to 200px max-width

--- frontend/src/pages/add-recipe/AddRecipe.js
+++ frontend/src/pages/add-recipe/AddRecipe.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addRecipe.css";
 
 function addInputToIngredient() {
-  const inputContainer = document.querySelector(".multipleIngredient-container");
+  const inputContainer = document.querySelector(
+    ".multipleIngredient-container"
+  );
   const input = document.createElement("input");
   input.type = "text";
   inputContainer.appendChild(input);
@@ -15,42 +17,57 @@ function addInputToSteps() {
   inputContainer.appendChild(input);
 }
 
-export default function addRecipe() {
+export default function AddRecipe() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const fileInputChange = (selectedImage) => {
+    setSelectedFile(selectedImage.target.files[0]);
+    setPreview(URL.createObjectURL(selectedImage.target.files[0]));
+  };
+
   return (
     <div className="recipe-page">
       <div className="add-recipe">
         <h1>Buzz in a Recipe!</h1>
         <div className="add-recipe-input">
-          <label className="recipe-step">Name: </label>
+          <label className="recipe-name">Name: </label>
           <input type="text"></input>
         </div>
         <div className="add-recipe-input">
- 
-          <label className="recipe-step">Description: </label>
+          <label className="recipe-description">Description: </label>
           <input type="text"></input>
         </div>
         <div className="add-recipe-input">
-
-          <label className="recipe-step">Ingredients: </label>
+          <label className="recipe-ingredients">Ingredients: </label>
           <div className="multipleIngredient-container">
             <input type="text"></input>
             <input type="text"></input>
             <input type="text"></input>
           </div>
-          <button className="extraInput" onClick={addInputToIngredient}>Add Ingredient</button>
+          <button className="extraInput" onClick={addInputToIngredient}>
+            Add Ingredient
+          </button>
         </div>
-        <div class="add-recipe-input">
-          <label className="recipe-step">Steps: </label>
+        <div className="add-recipe-input">
+          <label className="recipe-steps">Steps: </label>
           <div className="multipleSteps-container">
             <input type="text"></input>
             <input type="text"></input>
             <input type="text"></input>
           </div>
-          <button className="extraInput" onClick={addInputToSteps}>Add Step</button>
+          <button className="extraInput" onClick={addInputToSteps}>
+            Add Step
+          </button>
         </div>
-        <div>
-          <label className="recipe-step">Time to Cook: </label>
+        <div className="add-recipe-input">
+          <label className="recipe-time">Time to Cook: </label>
           <input type="text"></input>
+        </div>
+        <div className="add-recipe-input">
+          <label className="recipe-image">Upload an Image: </label>
+          <input type="file" onChange={fileInputChange}></input>
+          {preview && <img className="preview-image"src={preview} alt="Preview"/>}
         </div>
         <button>Add Recipe to Your Hive!</button>
       </div>
