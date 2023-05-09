5f82ee39e53d77f0a92a4f4c6ac294f4ad465932
Matthew Creek <matthew.creek27@gmail.com>
2023-05-08 20:48:09 -0500

    state is updating correctly to correspond to fields

--- frontend/src/pages/add-recipe/AddRecipe.js
+++ frontend/src/pages/add-recipe/AddRecipe.js
@@ -20,41 +20,48 @@ function addInputToSteps() {
 export default function AddRecipe() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [preview, setPreview] = useState(null);
+
   const [ingredientName, setIngredientName] = useState('');
   const [ingredientArr, setIngredientArr] = useState([]);
+  const [stepStr, setStepStr] = useState('');
+  const [stepArr, setStepArr] = useState([]);
+  const [recipeName, setRecipeName] = useState('');
+  const [description, setDescription] = useState('');
+  const [time, setTime] = useState('');
+
   const [formData, setFormData] = useState({
     recipeName: '',
     description: '',
-    ingredients: ingredientArr,
+    ingredients: [],
     steps: [],
     time: ''
   });
   
-  // console.log(ingredientName)
-  console.log(formData)
-  // console.log(ingredientArr)
+  
 
   const fileInputChange = (selectedImage) => {
     setSelectedFile(selectedImage.target.files[0]);
     setPreview(URL.createObjectURL(selectedImage.target.files[0]));
   };
 
-  function handleFormChange(event) {
-    const { name, value } = event.target;
-      setFormData((prevFormData) => {
-        return {
-          ...prevFormData,
-          [name]: value
-        }
-      })
+  function handleRecipeNameChange(event) {
+    const { value } = event.target;
+      setRecipeName(value)
+  }
+
+  function handleDescriptionChange(event) {
+    const { value } = event.target;
+      setDescription(value)
+  }
+
+  function handleTimeChange(event) {
+    const { value } = event.target;
+      setTime(value)
   }
 
   function handleIngredient(event) {
     const { value } = event.target;
-    setIngredientName((prevIngredientName) => {
-      return value
-      
-    })
+    setIngredientName(value)
   }
 
   function addIngredient() {
@@ -63,6 +70,32 @@ export default function AddRecipe() {
       ingredientName
     ])
   }
+
+  function handleStep(event) {
+    const { value } = event.target;
+    setStepStr(value)
+  }
+
+  function addStep() {
+    setStepArr([
+      ...stepArr,
+      stepStr
+    ])
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    setFormData({
+      recipeName: recipeName,
+      description: description,
+      ingredients: ingredientArr,
+      steps: stepArr,
+      time: time
+    })
+  }
+
+
+  // console.log(formData)
 
   return (
     <div className="recipe-page">
@@ -73,8 +106,8 @@ export default function AddRecipe() {
           <input 
             type="text"
             name="recipeName"
-            onChange={handleFormChange}
-            value={formData.recipeName}
+            onChange={handleRecipeNameChange}
+            value={recipeName}
           />
         </div>
         <div className="add-recipe-input">
@@ -82,8 +115,8 @@ export default function AddRecipe() {
           <input 
             type="text"
             name="description"
-            onChange={handleFormChange}
-            value={formData.description}
+            onChange={handleDescriptionChange}
+            value={description}
           />
         </div>
         <div className="add-recipe-input">
@@ -95,37 +128,44 @@ export default function AddRecipe() {
               onChange={handleIngredient}
               value={ingredientName}
             />
-            {/* <input type="text" />
-            <input type="text" /> */}
           </div>
-          {/* <button className="extraInput" onClick={addInputToIngredient}>
-            Add Ingredient
-          </button> */}
           <button className="extraInput" onClick={addIngredient}>Add Ingredient</button>
           <ul>
-            {ingredientArr.map(ingredient => (
-              <li>{ingredient}</li>
+            {ingredientArr.map((ingredient, index) => (
+              <>
+                <li key={index}>{ingredient}</li>
+                {/* <button>X</button> */}
+              </>
             ))}
           </ul>
         </div>
         <div className="add-recipe-input">
           <label className="recipe-steps">Steps: </label>
           <div className="multipleSteps-container">
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            <input 
+              type="text" 
+              name="step"
+              onChange={handleStep}
+              value={stepStr}
+            />
           </div>
-          <button className="extraInput" onClick={addInputToSteps}>
-            Add Step
-          </button>
+          <button className="extraInput" onClick={addStep}>Add Step</button>
+          <ul>
+            {stepArr.map((step, index) => (
+              <>
+                <li key={index}>{step}</li>
+                {/* <button>X</button> */}
+              </>
+            ))}
+          </ul>
         </div>
         <div className="add-recipe-input">
           <label className="recipe-time">Time to Cook: </label>
           <input 
             type="text" 
             name="time"
-            onChange={handleFormChange}
-            value={formData.time}
+            onChange={handleTimeChange}
+            value={time}
           />
         </div>
         <div className="add-recipe-input">
@@ -133,7 +173,7 @@ export default function AddRecipe() {
           <input type="file" onChange={fileInputChange}></input>
           {preview && <img className="preview-image"src={preview} alt="Preview"/>}
         </div>
-        <button>Add Recipe to Your Hive!</button>
+        <button onClick={handleFormSubmit}>Add Recipe to Your Hive!</button>
       </div>
     </div>
   );
