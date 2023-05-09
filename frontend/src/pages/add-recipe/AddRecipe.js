12743d7ccbd100c886223cafb9af8b2fe4b11bf4
Matthew Creek <matthew.creek27@gmail.com>
2023-05-08 19:52:21 -0500

    ingredient button working but not adding to overall state

--- frontend/src/pages/add-recipe/AddRecipe.js
+++ frontend/src/pages/add-recipe/AddRecipe.js
@@ -20,11 +20,49 @@ function addInputToSteps() {
 export default function AddRecipe() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [preview, setPreview] = useState(null);
+  const [ingredientName, setIngredientName] = useState('');
+  const [ingredientArr, setIngredientArr] = useState([]);
+  const [formData, setFormData] = useState({
+    recipeName: '',
+    description: '',
+    ingredients: ingredientArr,
+    steps: [],
+    time: ''
+  });
+  
+  // console.log(ingredientName)
+  console.log(formData)
+  // console.log(ingredientArr)
 
   const fileInputChange = (selectedImage) => {
     setSelectedFile(selectedImage.target.files[0]);
     setPreview(URL.createObjectURL(selectedImage.target.files[0]));
   };
+
+  function handleFormChange(event) {
+    const { name, value } = event.target;
+      setFormData((prevFormData) => {
+        return {
+          ...prevFormData,
+          [name]: value
+        }
+      })
+  }
+
+  function handleIngredient(event) {
+    const { value } = event.target;
+    setIngredientName((prevIngredientName) => {
+      return value
+      
+    })
+  }
+
+  function addIngredient() {
+    setIngredientArr([
+      ...ingredientArr,
+      ingredientName
+    ])
+  }
 
   return (
     <div className="recipe-page">
@@ -32,29 +70,50 @@ export default function AddRecipe() {
         <h1>Buzz in a Recipe!</h1>
         <div className="add-recipe-input">
           <label className="recipe-name">Name: </label>
-          <input type="text"></input>
+          <input 
+            type="text"
+            name="recipeName"
+            onChange={handleFormChange}
+            value={formData.recipeName}
+          />
         </div>
         <div className="add-recipe-input">
           <label className="recipe-description">Description: </label>
-          <input type="text"></input>
+          <input 
+            type="text"
+            name="description"
+            onChange={handleFormChange}
+            value={formData.description}
+          />
         </div>
         <div className="add-recipe-input">
           <label className="recipe-ingredients">Ingredients: </label>
           <div className="multipleIngredient-container">
-            <input type="text"></input>
-            <input type="text"></input>
-            <input type="text"></input>
+            <input 
+              type="text" 
+              name="ingredient"
+              onChange={handleIngredient}
+              value={ingredientName}
+            />
+            {/* <input type="text" />
+            <input type="text" /> */}
           </div>
-          <button className="extraInput" onClick={addInputToIngredient}>
+          {/* <button className="extraInput" onClick={addInputToIngredient}>
             Add Ingredient
-          </button>
+          </button> */}
+          <button className="extraInput" onClick={addIngredient}>Add Ingredient</button>
+          <ul>
+            {ingredientArr.map(ingredient => (
+              <li>{ingredient}</li>
+            ))}
+          </ul>
         </div>
         <div className="add-recipe-input">
           <label className="recipe-steps">Steps: </label>
           <div className="multipleSteps-container">
-            <input type="text"></input>
-            <input type="text"></input>
-            <input type="text"></input>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
           </div>
           <button className="extraInput" onClick={addInputToSteps}>
             Add Step
@@ -62,7 +121,12 @@ export default function AddRecipe() {
         </div>
         <div className="add-recipe-input">
           <label className="recipe-time">Time to Cook: </label>
-          <input type="text"></input>
+          <input 
+            type="text" 
+            name="time"
+            onChange={handleFormChange}
+            value={formData.time}
+          />
         </div>
         <div className="add-recipe-input">
           <label className="recipe-image">Upload an Image: </label>
