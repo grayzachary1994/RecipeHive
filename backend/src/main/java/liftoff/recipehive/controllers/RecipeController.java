4572b3dde1eace0e1d9f241a8e37454eec404040
Amy Stockhausen <stockhausenamy@gmail.com>
2023-05-18 12:44:40 -0500

    Successfully passing username with recipe into database

--- backend/src/main/java/liftoff/recipehive/controllers/RecipeController.java
+++ backend/src/main/java/liftoff/recipehive/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package liftoff.recipehive.controllers;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import liftoff.recipehive.models.Recipe;
 import liftoff.recipehive.repositories.RecipeRepository;
-import liftoff.recipehive.security.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,10 +20,10 @@ import java.util.List;
 public class RecipeController {
 
     @Autowired
-    JwtUtils jwtUtils;
-
-    @Autowired
     private RecipeRepository recipeRepository;
+
+    @Value("${bezkoder.app.jwtSecret}")
+    private String jwtSecret;
 
 
     //    @GetMapping
@@ -29,11 +32,11 @@ public class RecipeController {
 //        return "add-recipe";
 //    }
     @PostMapping("add-recipe")
-    public String processAddRecipeForm(@RequestBody @Valid Recipe newRecipe, Errors errors, String accessToken) {
+    public String processAddRecipeForm(@RequestBody @Valid Recipe newRecipe, Errors errors, HttpServletRequest httpServletRequest) {
         if (errors.hasErrors()) {
             return "add-recipe";
         }
-        String newRecipeUserName = jwtUtils.getUserNameFromJwtToken(accessToken);
+        String newRecipeUserName = retrieveUsernameFromHeader(httpServletRequest);
         newRecipe.setRecipeUserName(newRecipeUserName);
         recipeRepository.save(newRecipe);
         return "Recipe added to your cookbook.";
@@ -44,6 +47,24 @@ public class RecipeController {
     public ResponseEntity<List<Recipe>> displayCookbook() {
         List<Recipe> recipes = recipeRepository.findAll();
         return ResponseEntity.ok(recipes);
+    }
+
+    public String retrieveUsernameFromHeader(HttpServletRequest httpServletRequest) {
+        System.out.println("Starting retrieval");
+        String sessionUserName = httpServletRequest.getHeader("Authorization");
+        System.out.println(sessionUserName + "-1");
+        if (StringUtils.hasText(sessionUserName) && sessionUserName.startsWith("Bearer ")) {
+            String longUserName = sessionUserName;
+            sessionUserName = longUserName.substring(7, longUserName.length());
+        }
+        System.out.println(sessionUserName + "-2");
+
+        String decodedSessionUserName = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(sessionUserName).getBody().getSubject();
+
+        System.out.println(decodedSessionUserName + "-3");
+
+        System.out.println("Executed retrieval");
+        return decodedSessionUserName;
     }
 }
 
